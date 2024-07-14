c47143de5156f51dd49f86871ca9e9bff22dac54
TimeCodings <youtube@timecoding.de>
2024-07-14 11:52:10 +0200

    1.21 Update

--- src/main/java/dev/timecoding/onebedperworld/OneBedPerWorld.java
+++ src/main/java/dev/timecoding/onebedperworld/OneBedPerWorld.java
@@ -1,14 +1,13 @@
 package dev.timecoding.onebedperworld;
 
-import dev.timecoding.onebedperworld.api.AutoUpdater;
 import dev.timecoding.onebedperworld.api.Metrics;
 import dev.timecoding.onebedperworld.command.OBPWCommand;
 import dev.timecoding.onebedperworld.command.OBPWCompleter;
 import dev.timecoding.onebedperworld.config.ConfigHandler;
 import dev.timecoding.onebedperworld.config.DataSaveHandler;
 import dev.timecoding.onebedperworld.listener.BedListener;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OneBedPerWorld extends JavaPlugin {
@@ -16,8 +15,6 @@ public final class OneBedPerWorld extends JavaPlugin {
     private static OneBedPerWorld plugin;
     private DataSaveHandler dataSaveHandler = new DataSaveHandler(this);
     private ConfigHandler configHandler = new ConfigHandler(this);
-
-    private AutoUpdater updater = null;
 
     @Override
     public void onEnable() {
@@ -29,10 +26,6 @@ public final class OneBedPerWorld extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new BedListener(), plugin);
         getCommand("onebedperworld").setExecutor(new OBPWCommand());
         getCommand("onebedperworld").setTabCompleter(new OBPWCompleter());
-
-        updater = new AutoUpdater(this);
-        updater.checkForPluginUpdate();
-        updater.checkForConfigUpdate();
 
         if(configHandler.getConfig().getBoolean("bStats")) {
             Metrics bstats = new Metrics(this, 16015);
